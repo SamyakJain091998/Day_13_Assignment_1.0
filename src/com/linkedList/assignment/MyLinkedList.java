9997001535541ff7e7024738336c28ea2f693d22
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-10-07 20:59:45 +0530

    Modified: UC10 executed successfully in UC10 class along with MyNode class for LinkedList node creation, INode interface which is implemented by MyNode.java, MyLinkedListTest class for testing and MyLinkedList class for implementing the linkedList functions

---
+++ src/com/linkedList/assignment/MyLinkedList.java
@@ -0,0 +1,66 @@
+package com.linkedList.assignment;
+
+public class MyLinkedList {
+	public INode head;
+	public INode tail;
+
+	MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void addToBack(INode newNode) {
+		// TODO Auto-generated method stub
+		if (tail == null) {
+			this.tail = newNode;
+		}
+		if (head == null) {
+			this.head = newNode;
+		} else {
+			INode tempNode = this.tail;
+			this.tail = newNode;
+			tempNode.setNext(tail);
+		}
+	}
+
+	public void SortedAdding(MyNode<Integer> currentNode) {
+		// TODO Auto-generated method stub
+		if (tail == null) {
+			this.tail = currentNode;
+		}
+		if (head == null) {
+			this.head = currentNode;
+		} else {
+			INode tempNode = head;
+			INode previousNode = head;
+			if (currentNode.getKey().compareTo((Integer) tempNode.getKey()) > 0) {
+				while (currentNode.getKey().compareTo((Integer) tempNode.getKey()) > 0 && tempNode.getNext() != null) {
+					previousNode = tempNode;
+					tempNode = tempNode.getNext();
+				}
+				if (currentNode.getKey().compareTo((Integer) tempNode.getKey()) < 0) {
+					INode frontNode = tempNode;
+					currentNode.setNext(frontNode);
+					previousNode.setNext(currentNode);
+				} else {
+					tempNode.setNext(currentNode);
+					tail = currentNode;
+				}
+			} else {
+				currentNode.setNext(head);
+				head = currentNode;
+			}
+		}
+	}
+	
+	public void printMyNodes() {
+		INode tempNode = head;
+		while (tempNode != tail) {
+			System.out.print(tempNode.getKey());
+			if (!tempNode.equals(tail))
+				System.out.print(" -> ");
+			tempNode = tempNode.getNext();
+		}
+		System.out.print(tempNode.getKey());
+	}
+}
---
+++ src/com/linkedList/assignment/MyLinkedListTest.java
@@ -0,0 +1,41 @@
+package com.linkedList.assignment;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+	@Test
+	public void InsertingAnElementInASortedOrderInLinkedList() {
+		System.out.println("----TASK----");
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(40);
+		MyNode<Integer> myFourthNode = new MyNode<>(70);
+		
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.SortedAdding(myFirstNode);
+		myLinkedList.SortedAdding(mySecondNode);
+		myLinkedList.SortedAdding(myThirdNode);
+		myLinkedList.SortedAdding(myFourthNode);
+		
+		MyLinkedList checkLinkedList = new MyLinkedList();
+		MyNode<Integer> myFirstNode1 = new MyNode<>(56);
+		MyNode<Integer> mySecondNode2 = new MyNode<>(30);
+		MyNode<Integer> myThirdNode3 = new MyNode<>(46);
+		MyNode<Integer> myFourthNode4 = new MyNode<>(70);
+		checkLinkedList.addToBack(myFirstNode1);
+		checkLinkedList.addToBack(mySecondNode2);
+		checkLinkedList.addToBack(myThirdNode3);
+		checkLinkedList.addToBack(myFourthNode4);
+		
+		System.out.println("Unsorted linked list is : ");
+		checkLinkedList.printMyNodes();
+		System.out.println();
+		System.out.println("Linked list after sorting is : ");
+//		boolean isInserted = myLinkedList.InsertingAnElementAfterAnElementInALinkedList(30, newNode);
+		myLinkedList.printMyNodes();
+		System.out.println();
+		System.out.println();
+//		Assert.assertEquals(true, isInserted);
+	}
+}
