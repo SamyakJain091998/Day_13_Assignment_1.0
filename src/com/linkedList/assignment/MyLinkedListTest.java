dc773398baf98c2720abdf00920fb07299bdd620
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-10-07 18:52:02 +0530

    Modified: UC4 executed successfully in UC4 class along with MyNode class for LinkedList node creation, INode interface which is implemented by MyNode.java, MyLinkedListTest class for testing and MyLinkedList class for implementing the linkedList functions

---
+++ src/com/linkedList/assignment/MyLinkedListTest.java
@@ -0,0 +1,27 @@
+package com.linkedList.assignment;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+	@Test
+	public void insertANodeInBetweenTwoNodes() {
+		System.out.println("----TASK 3----");
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.addToBack(myFirstNode);
+		// myLinkedList.addToBack(mySecondNode);
+		myLinkedList.addToBack(myThirdNode);
+
+		myLinkedList.insertInBetweenTwoNodes(myFirstNode, mySecondNode);
+		myLinkedList.printMyNodes();
+		System.out.println();
+		System.out.println();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+}
