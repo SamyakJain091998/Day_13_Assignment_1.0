c186b05bcdf27508753ec5b889e669627e2d3605
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-10-07 19:01:49 +0530

    Modified: UC5 executed successfully in UC5 class along with MyNode class for LinkedList node creation, INode interface which is implemented by MyNode.java, MyLinkedListTest class for testing and MyLinkedList class for implementing the linkedList functions

---
+++ src/com/linkedList/assignment/MyLinkedListTest.java
@@ -0,0 +1,27 @@
+package com.linkedList.assignment;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+	@Test
+	public void DeleteTheFirstNodeOfALinkedList() {
+		System.out.println("----TASK----");
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.addToBack(myFirstNode);
+		myLinkedList.addToBack(mySecondNode);
+		myLinkedList.addToBack(myThirdNode);
+
+		INode<Integer> deletedFirstNode = myLinkedList.deleteFirstElementInLinkedList();
+		System.out.println("Deleted node is : " + deletedFirstNode.getKey());
+		myLinkedList.printMyNodes();
+		System.out.println();
+		System.out.println();
+		boolean result = myLinkedList.head.equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+}
